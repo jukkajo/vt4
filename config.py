c33e99ee33d456ebf6ab0b078e2b1cff9f07685a
jii <76208694+jukkajo@users.noreply.github.com>
2023-01-15 04:16:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
-GOOGLE_CLIENT_ID = "982839663196-o02225ludpoie8up2j5q4pnn91t5sq13.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_CLIENT_ID = ""
+GOOGLE_CLIENT_SECRET = ""
